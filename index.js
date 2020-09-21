90c1246eb2dad0860440d2b5699821b9e95c2858
Ilya Maximov <mail@overlisted.net>
2020-09-21 12:25:20 +0200

    move player movement keys to play.js

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-import {keyListeners, mousePosition} from "/input.js"
 import editorLoop from "/editor.js"
 import playLoop from "/play.js"
-
-keyListeners.push({
-  keys: ["d"],
-  function: () => world.movePlayer([world.player.position[0] + 1, world.player.position[1]])
-});
-keyListeners.push({
-  keys: ["a"],
-  function: () => world.movePlayer([world.player.position[0] - 1, world.player.position[1]])
-});
-keyListeners.push({
-  keys: [" "],
-  function: () => world.movePlayer([world.player.position[0], world.player.position[1] - 2])
-});
-keyListeners.push({
-  keys: ["n"],
-  function: () => {
-    world.objects.push(new WorldObject({
-      classes: ["block"],
-      start: [world.player.position[0], world.player.position[1] + 1],
-      size: [2, 2],
-      color: "#666"
-    }, world));
-  }
-});
-
-addEventListener("keydown", e => {
-  if(e.key == "F6") debug = !debug;
-})
 
 let loop = () => console.error("No loop!");
 
--- play.js
+++ play.js
@@ -1,6 +1,35 @@
 import {renderPlayer, clearScreen} from "/render.js"
 import {doInput} from "/input.js"
 import {objectProcessors, world, World, WorldObject, setWorld} from "/library.js"
+import {keyListeners, mousePosition} from "/input.js"
+
+keyListeners.push({
+  keys: ["d"],
+  function: () => world.movePlayer([world.player.position[0] + 1, world.player.position[1]])
+});
+keyListeners.push({
+  keys: ["a"],
+  function: () => world.movePlayer([world.player.position[0] - 1, world.player.position[1]])
+});
+keyListeners.push({
+  keys: [" "],
+  function: () => world.movePlayer([world.player.position[0], world.player.position[1] - 2])
+});
+keyListeners.push({
+  keys: ["n"],
+  function: () => {
+    world.objects.push(new WorldObject({
+      classes: ["block"],
+      start: [world.player.position[0], world.player.position[1] + 1],
+      size: [2, 2],
+      color: "#666"
+    }, world));
+  }
+});
+
+addEventListener("keydown", e => {
+  if(e.key == "F6") debug = !debug;
+})
 
 async function downloadWorld(url) {
   const worldFile = await fetch(url);
