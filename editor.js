d089d19dba575979c4fc02815ca8e7792a97463a
Ilya Maximov <mail@overlisted.net>
2020-09-23 10:32:52 +0200

    add basic input and a key to the editor

--- editor.js
+++ editor.js
@@ -1,10 +1,36 @@
 import {clearScreen} from "/render.js"
 import {objectProcessors, world, setWorld, World, WorldObject} from "/library.js"
+import {keyListeners, mousePosition, doInput} from "/input.js"
+
+function randomMax(max) {
+  return Math.floor(Math.random() * 255);
+}
+
+function randomRange(min, max) {
+  let result = randomMax(max);
+  while(result > min) result = randomMax(max);
+
+  return result;
+}
+
+keyListeners.push({
+  keys: ["n"],
+  function: () => {
+    world.objects.push(new WorldObject({
+      classes: ["block"],
+      start: [mousePosition[0] / 10, mousePosition[1] / 10],
+      size: [2, 2],
+      color: `rgb(${randomMax(255)}, ${randomMax(255)}, ${randomMax(255)})`
+    }, world));
+  }
+});
 
 export default async function loop() {
   clearScreen();
 
   if(!world) setWorld(new World());
+
+  doInput();
 
   for(const processor of objectProcessors) {
     const objects = world.getObjectsWithClasses(processor.classes);
